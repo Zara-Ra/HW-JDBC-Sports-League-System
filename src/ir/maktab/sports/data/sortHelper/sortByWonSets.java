17aba57ea5538e0ff2fabb3a6ddb056228930e5f
zahra.rahimi <zahra.rahimi@gmail.com>
2022-09-05 23:14:09 +0430

    sort by won set debuged

--- src/ir/maktab/sports/data/sortHelper/sortByWonSets.java
+++ src/ir/maktab/sports/data/sortHelper/sortByWonSets.java
@@ -1,12 +1,15 @@
 package ir.maktab.sports.data.sortHelper;
 
+import ir.maktab.sports.data.team.Team;
 import ir.maktab.sports.data.team.VolleyballTeam;
 
 import java.util.Comparator;
 
-public class sortByWonSets implements Comparator<VolleyballTeam> {
+public class sortByWonSets implements Comparator<Team> {
     @Override
-    public int compare(VolleyballTeam vt1, VolleyballTeam vt2) {
+    public int compare(Team team1, Team team2) {
+        VolleyballTeam vt1 = (VolleyballTeam) team1;
+        VolleyballTeam vt2 = (VolleyballTeam) team1;
         int [] sets1 = vt1.getSets();
         int [] sets2 = vt2.getSets();
         return sets2[0] - sets1[0];
